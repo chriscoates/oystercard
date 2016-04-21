b763d67eba3d78ad289d68cacb3a97cebb50cfd7
Chris Coates <coates.c@hotmail.com>
2016-04-21 18:02:15 +0100

    updates to oystercard class

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,35 +1,36 @@
 # require 'journey'
 
 class Oystercard
-  MAXIMUM_BALANCE = 90
+  MAXIMUM_BALANCE = 31
+  MINIMUM_FARE = 1
 
   attr_reader :balance, :journeys, :journey_log
 
   def initialize
     @balance = 0
-    # @journeys = []
   end
 
   def top_up(amount)
-    raise "Cannot exceed the limit of #{MAXIMUM_BALANCE}" if balance + amount > MAXIMUM_BALANCE
+    fail "Maximum balance of #{MAXIMUM_BALANCE} exceeded" if balance + amount > MAXIMUM_BALANCE
     @balance += amount
   end
 
   def touch_in(station, journey_log)
-    message = "Cannot touch in: balance must be at least #{journey::MINIMUM_FARE}"
-    raise message if balance < journey::MINIMUM_FARE
-    @journey_log = journey_log.new
-    @journey_log.start_journey(station)
+    fail "Not enough balance on card" if balance < MINIMUM_FARE
   end
 
   def touch_out(station)
-    @journeys << @journey.finish_journey(station)
-    deduct(@journey.fare) ######
+    deduct(MINIMUM_FARE)
+    @entry_station = nil
+  end
+
+  def in_journey?
+   !!entry_station
   end
 
   private
+  def deduct(fare)
+    @balance -= fare
+  end
 
-    def deduct(amount)
-      @balance -= amount
-    end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,68 +1,94 @@
-require 'oystercard'
-require 'journey'
+require "oystercard"
 
 describe Oystercard do
-  let(:journey) { Journey }
-  subject(:oystercard) { described_class.new }
-  let(:entry_station) { double :station }
-  let(:exit_station) { double :station }
+  let(:entry_station) { double(:entry_station) }
+  let(:exit_station) { double(:exit_station) }
 
-  context "when initialized" do
-    it "has a balance equals to 0" do
-      expect(oystercard.balance).to eq 0
+  let(:loaded_card) do
+    loaded_card = Oystercard.new
+    loaded_card.top_up(Oystercard::MAXIMUM_BALANCE)
+    loaded_card
+  end
+  let(:station) {double (:station)}
+
+  context "when new card issues" do
+    it "has initial balance" do
+      expect(subject.balance).to eq(0)
     end
   end
 
-	describe "#top_up" do
-		it "adds money to its balance" do
-			expect { oystercard.top_up 1 }.to change{ oystercard.balance }.by 1
-		end
+  context "#top_up" do
+    it "tops up the balance" do
+      expect{ subject.top_up 30 }.to change{ subject.balance }.by 30
+    end
+    it "raises an error if the maximum balance is exceeded" do
+      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      subject.top_up maximum_balance
+      error_maximum = "Maximum balance of #{Oystercard::MAXIMUM_BALANCE} exceeded"
+      expect{ subject.top_up 1 }.to raise_error error_maximum
+    end
+  end
 
-		it "raises error if exceeding maximum balance" do
-		  oystercard.top_up described_class::MAXIMUM_BALANCE
-		  expect { oystercard.top_up 1 }.to raise_error "Cannot exceed the limit of #{described_class::MAXIMUM_BALANCE}"
-		end
-	end
+  context "when touch in" do
+    it "requires a minimal balance of £1" do
+      expect{ subject.touch_in(station) }.to raise_error "Not enough balance on card"
+    end
+  end
 
-	describe "#touch_in" do
-		before do
-			oystercard.top_up 1
-			oystercard.touch_in(entry_station, journey)
-		end
+  context "charging the customer" do
+    it "deducts minimum fare when touch_out" do
+      expect{ loaded_card.touch_out(exit_station) }.to change{ loaded_card.balance }.by -Oystercard::MINIMUM_FARE
+    end
+  end
 
-		it "stores the entry station" do
-			expect(oystercard.journey.entry_station).to eq entry_station
-		end
+  context "add touch_in and out" do
+    it "can touch in" do
+      loaded_card.touch_in(entry_station)
+      expect(loaded_card).to be_in_journey
+    end
 
-		it 'will not touch in if below minimum balance' do
-			allow(oystercard).to receive(:balance).and_return 0
-			message = "Cannot touch in: balance must be at least #{Journey::MINIMUM_FARE}"
-			expect { oystercard.touch_in(entry_station, journey) }.to raise_error message
-		end
-	end
+    it "can touch out" do
+      loaded_card.touch_in(entry_station)
+      loaded_card.touch_out(exit_station)
+      expect(loaded_card).not_to be_in_journey
+    end
+  end
 
-	describe "#touch_out" do
-		before do
-			oystercard.top_up 1
-			oystercard.touch_in(entry_station, journey)
-		end
+  context " history journeys " do
+    # it 'stores exit station' do
+    #   loaded_card.touch_in(entry_station)
+    #   loaded_card.touch_out(exit_station)
+    #   expect(loaded_card.exit_station).to eq exit_station
+    # end
 
-		it "stores the exit station" do
-			oystercard.touch_out(exit_station)
-			expect(oystercard.journey.exit_station).to eq exit_station
-		end
+    it "remembers the entry_station when touch in" do
+      loaded_card.touch_in(entry_station)
+      expect(loaded_card.entry_station).to eq entry_station
+    end
 
-		it "reduces the balance by minimum fare" do
-			expect { oystercard.touch_out(exit_station) }.to change{ oystercard.balance }.by -(Journey::MINIMUM_FARE)
-		end
-	end
+    it "forgets the entry station when touch out" do
+      loaded_card.touch_in(entry_station)
+      loaded_card.touch_out(exit_station)
+      expect(loaded_card.entry_station).to be nil
+    end
+  end
 
-	describe "touching in and out" do
-		it "creates one journey"do
-			oystercard.top_up 1
-			oystercard.touch_in(entry_station, journey)
-			oystercard.touch_out(exit_station)
-			expect(oystercard.journeys). to include journey
-		end
-	end
+  context "saving previous journeys" do
+    let(:test_journey){ {entry_station: entry_station, exit_station: exit_station} }
+    before(:each) do
+      loaded_card.touch_in(entry_station)
+      loaded_card.touch_out(exit_station)
+    end
+
+    # it "stores one journey" do
+    #   expect(loaded_card.journey.keys).to include :entry_station
+    #   expect(loaded_card.journey.values).to include exit_station
+    # end
+
+    it "adds the current journey to the journey history" do
+      #test_journey = Hash.new
+      #test_journey[:entry_station] = exit_station
+      expect(loaded_card.journeys.last).to eq test_journey
+    end
+  end
 end
